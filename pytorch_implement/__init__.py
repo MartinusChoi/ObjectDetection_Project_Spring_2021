de24b5fbed5db15c1eb0ad09d92f7c78f590dd23
youngminChoi <martinus.choi@gmail.com>
2021-05-18 02:47:23 +0900

    removed unuse directory | IndexError in build_targets() in loss.py

--- pytorch_implement/__init__.py
+++
@@ -1,2 +0,0 @@
-# enable use python 3 code in python 2
-from __future__ import division
--- pytorch_implement/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# enable use python 3 code in python 2
-from __future__ import division
-
-from .layers import EmptyLayer, DetectionLayer
-from .parse_config import parse_cfg
-from .utils import unique, predict_transform, write_result, bbox_iou, weights_init_normal
